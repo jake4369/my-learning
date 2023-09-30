b6e02c32e576dabaf701092c8c4b8bca3b666944
Jake Samuels <samuels242@googlemail.com>
2023-09-30 15:55:44 +0100

    Import sing along section

--- app/English/Phonics/[set]/[letter]/page.jsx
+++ app/English/Phonics/[set]/[letter]/page.jsx
@@ -3,24 +3,25 @@
 import { useParams } from "next/navigation";
 import Image from "next/image";
 
-import letterData from "./../../../../../data/letters.json";
+import data from "./../../../../../data/letters.json";
 
 import Soundcard from "@components/Phonics/Soundcard/Soundcard";
 import TracingComponent from "@components/Phonics/TracingComponent";
 import SoundingSection from "@components/Phonics/Sounding/SoundingSection";
 import FlashcardSection from "@components/Shared/Flashcards/FlashcardSection";
+import SingAlongSection from "@components/Phonics/SingAlong/SingAlongSection";
 
 const page = () => {
   const { letter } = useParams();
 
-  const color = letterData[0][letter].color;
+  const letterData = data[0][letter];
 
   return (
     <div className="letter-page">
       <h1
         className="letter-page__letter"
         style={{
-          backgroundColor: color,
+          backgroundColor: letterData.color,
         }}
       >
         {letter}
@@ -30,7 +31,7 @@ const page = () => {
         <h2>Story Time</h2>
 
         <Image
-          src={letterData[0][letter].storyImgUrl}
+          src={letterData.storyImgUrl}
           alt=""
           width={100}
           height={100}
@@ -38,26 +39,30 @@ const page = () => {
           priority={true}
         />
 
-        <p>{letterData[0][letter].story}</p>
+        <p>{letterData.story}</p>
       </section>
 
-      <Soundcard letter={letter} color={color} />
+      <Soundcard letter={letter} color={letterData.color} />
 
       <TracingComponent
         letter={letter}
         width={400}
         height={400}
-        color={color}
+        color={letterData.color}
       />
 
-      <SoundingSection
-        letter={letter}
-        arr={letterData[0][letter].soundingImages}
-      />
+      <SoundingSection letter={letter} arr={letterData.soundingImages} />
 
       <FlashcardSection
-        arr={letterData[0][letter].flashcardWords}
-        color={color}
+        arr={letterData.flashcardWords}
+        color={letterData.color}
+      />
+
+      <SingAlongSection
+        img={letterData.songImg}
+        song={letterData.song}
+        arr={letterData.songLyrics}
+        color={letterData.color}
       />
     </div>
   );
